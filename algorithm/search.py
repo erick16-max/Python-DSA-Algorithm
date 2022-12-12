dea2d4365df3586bba441bddea911cc6321c5f63
erick16-max <erickgege16@gmail.com>
2022-12-12 15:46:36 +0300

    binary search

--- algorithm/search.py
+++ algorithm/search.py
@@ -1,4 +1,5 @@
 from jovian.pythondsa import evaluate_test_cases, evaluate_test_case
+import jovian
 
 tests = [
 
@@ -20,6 +21,8 @@ def linear_search(cards, query):
     #         return index
     #     index += 1
     # return -1
+
+    #gegerick algorithm
     if len(cards) < 1:
         return -1
 
@@ -27,9 +30,64 @@ def linear_search(cards, query):
         if (cards[index] == query):
             return index
     return -1
+#testing all the test cases including edge cases
+# evaluate_test_cases(linear_search, tests)
+
+
+#BINARY SEARCH
+#jovian algorithm
+def test_location(cards, query, mid):
+    mid_number = cards[mid]
+    print("mid:", mid, ", mid_number:", mid_number)
+    if mid_number == query:
+        if mid-1 >= 0 and cards[mid-1] == query:
+            return 'left'
+        else:
+            return 'found'
+    elif mid_number < query:
+        return 'left'
+    else:
+        return 'right'
+
+def locate_card(cards, query):
+    lo, hi = 0, len(cards) - 1
+    
+    while lo <= hi:
+        print("lo:", lo, ", hi:", hi)
+        mid = (lo + hi) // 2
+        result = test_location(cards, query, mid)
+        
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        elif result == 'right':
+            lo = mid + 1
+    return -1
+
+#gegerick algo
+def binary_search(cards,query):
+    
+    max_num = len(cards) - 1
+    min_num = 0
+    while min_num <= max_num:
+        mid = (max_num+min_num) // 2
+        if cards[mid] == query:
+            if mid-1 >= 0 and cards[mid-1] == query:
+                #apply recursion to find the first occurence of query
+                return binary_search(cards[:mid], query)
+            return mid
+        elif cards[mid] < query:
+            max_num = mid - 1
+        elif cards[mid] > query:
+            min_num = mid + 1
+    return -1
+
+
+evaluate_test_cases(binary_search,tests)
+
+
 
    
 
-     
-
-evaluate_test_cases(linear_search, tests)
+# jovian.commit(project='python-binary-search', environment=None)
